9515fe9f6b0c56c4748e8b7c71317e85a1fbfdd7
Utkarsh jain <87333434+Utkarshjain070@users.noreply.github.com>
2025-08-05 01:46:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ if st.session_state["page"] == "Home":
         </div>
     """, unsafe_allow_html=True)
     
-    col1, col2, col3 = st.columns([2,1,2])
+    col1, col2, col3 = st.columns([1,1,1])
     with col1:
         if st.button("Predict Heart Disease ❤️", use_container_width=True):
             st.session_state["page"] = "Heart Disease Prediction"
@@ -349,6 +349,7 @@ elif st.session_state["page"] == "Heart Disease Prediction":
             st.session_state["page"] = "Home"
     
     
+
 
 
 
